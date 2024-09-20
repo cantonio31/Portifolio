865a7fcab39f5f1fc9968f50a89c40b30a6fe925
cantonio31 <cantonio31@gmail.com>
2024-09-20 15:31:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-   
-    const form = document.querySelector("form");
-    form.addEventListener("submit", (event) => {
-        event.preventDefault(); 
-        alert("Formulário enviado com sucesso!");
-    });
-});
+let carlos = document.getElementById('carlos');
+    carlos.innerHTML = `<label class="sobreCarlos">
+    <center>
+Profissional de Tecnologia da Informação, Funcionario da <a href="https://phone2b.com.br/" target="_blank"> Phone2b</a> e  <a href="https://runtask.com" target="_blank">Runtask</a>.<br>
+Conhecimento avaçando em manutenção em computadores, instalação de softwares, instalação/configuração de ramal, configuração PABX Panasonic<br>
+Manutenção de aparelhos AVAYA. Instalação de telefones digitais e analógicos, configuração de aparelhos telefônicos CISCO e AVAYA.<br>
+configuração de Active Directory, criação e adequação de políticas, configuração de DHCP, DNS e SWITCH, criação de usuários, <br>
+configuração de CRM, Firewall (SonicWall), impressora, montagem de infraestrutura de rede, <br>
+manutenção de redes e telefonia. Passagem de cabeamentos de rede, configuração de layout, organização de cabos no SWITCH. Manutenção rede elétrica.<br>
+Gerenciamento de equipe, montagem de patch panel, acompanhamento de obras, cotação de equipamentos. <br>
+Manutenção de desktop, notebooks, instalação e configuração de sistemas operacionais, atendimento a usuários, instalação de softwares, <br>
+passagem de cabeamentos, cripagem de cabos, organização de patch. Sistemas Operacionais, configuração relógio de ponto Dimep.<br>
+</center>
+ </label>`;
